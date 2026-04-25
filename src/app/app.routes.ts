9717276886ce6a8bb1367a337a577c7be5364c7e
Juan Mesa <juan.mesa655@pascualbravo.edu.co>
2026-04-25 00:42:14 -0500

    eliminar carpeta src obsoleta de la raiz

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'pelicula/:id', component: MovieDetailComponent },
-  { path: '**', redirectTo: '' },
-];
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  apiKey: 'd8243523f17394029c3863279848608c',
-  baseUrl: 'https://api.themoviedb.org/3',
-  imageUrl: 'https://image.tmdb.org/t/p/w500',
-  language: 'es-ES',
-};
